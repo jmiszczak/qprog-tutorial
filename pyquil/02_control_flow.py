4f28c48e89aded14074cad53d20ace70f163ec30
Jarek Miszczak <miszczak@iitis.pl>
2019-03-08 13:00:32 +0100

    Control flow example updated for pyQuil 2

--- pyquil/02_control_flow.py
+++ pyquil/02_control_flow.py
@@ -1,27 +1,25 @@
-from pyquil.api import QVMConnection
-from pyquil.quil import Program
+from pyquil import Program, get_qc
 from pyquil.gates import H, X, I, CNOT
-
 import numpy as np
 
-qvm = QVMConnection() # use the simulator
+qvm = get_qc('2q-qmv') # use the QVM simulator, 2 qubits
+prg = Program() # main program
+cond = prg.declare('ro', 'BIT', 1) # classical memory for condition
+
 pr = 0 # we use quantum coins to make decisions
 ar = 1 # the second register is for actual game
-cregs = [ar, pr] # our classical registers
 
+# two branches
 dec0 = Program(X(ar)) # flip
 dec1 = Program(I(ar)) # do nothing
 
-prog = Program()
-prog.inst(X(ar))#, H(pr)) # initialize to heads
-prog.inst(H(pr)) # for decision making
-prog.inst(H(ar)) # flip
-prog.measure(pr, pr)
-prog.if_then(pr, dec1, dec0)
-prog.inst(H(ar)) # undo the flip
-prog.measure(ar,ar)
+# construct the main program
+prg.inst(X(ar)) # initialize to heads
+prg.inst(H(pr)) # for decision making
 
-print(qvm.wavefunction(prog))
+# make the decision
+prg.measure(pr, out).if_then(out, dec1, dec0)
 
-res = qvm.run(prog, cregs, trials = 10)
+exe = qvm.compile(prg)
+res = qvm.run(exe)
 print(res)
